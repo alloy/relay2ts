06cb3030167dfc30f9b1827c21e2019487272c99
Eloy Durán <eloy.de.enige@gmail.com>
2017-01-18 15:28:18 -0500

    [bin] Add proper CLI

--- src/bin.ts
+++ src/bin.ts
@@ -2,16 +2,94 @@
 
 import * as fs from 'fs'
 import * as GraphQL from 'graphql'
+import * as minimist from 'minimist'
+import * as findPackage from 'find-package-json'
+
 import { generateRelayFragmentsInterface } from './index'
 
-const schemaSource: string = fs.readFileSync(process.argv[2], { encoding: 'utf-8' })
-const schemaJSON = JSON.parse(schemaSource)
-const schema = GraphQL.buildClientSchema(schemaJSON.data || schemaJSON)
+function banner() {
+  console.log(`
+Usage: ${process.argv[1]} --schema=path/to/schema.json [...FILES]
 
-const files = process.argv.slice(3, process.argv.length)
-files.forEach(file => {
-  const source: string = fs.readFileSync(file, { encoding: 'utf-8' })
-  console.log(file)
-  console.log(generateRelayFragmentsInterface(schema, source))
-  console.log('')
-})
+    --schema   The path to a GraphQL schema json file that was generated with an introspection query.
+    --list     Print out the props interfaces for all FILES that contain Relay query fragments (the default).
+    --update   Updates files to add the props interface.
+    --version  The version of this tool.
+    --help     This text.
+  `.trim())
+}
+
+function fail(message: string) {
+  console.log(message)
+  process.exit(1)
+}
+
+interface ARGV {
+  _: string[],
+  schema?: string,
+  help?: boolean,
+  version?: boolean,
+  list?: boolean,
+  update?: boolean,
+}
+
+const argv: ARGV = minimist(process.argv.slice(2))
+
+if (argv.help) {
+  banner()
+  process.exit(0)
+}
+
+if (argv.version) {
+  console.log(findPackage(__dirname).next().value.version)
+  process.exit(0)
+}
+
+if (!argv.schema || argv._.length === 0) {
+  banner()
+  process.exit(1)
+}
+
+if (!fs.existsSync(argv.schema)) {
+  fail('The schema doesn’t exist at the provided path.')
+}
+
+let schema
+try {
+  const schemaSource: string = fs.readFileSync(argv.schema, { encoding: 'utf-8' })
+  const schemaJSON = JSON.parse(schemaSource)
+  schema = GraphQL.buildClientSchema(schemaJSON.data || schemaJSON)
+}
+catch(error) {
+  fail(`Unable to read schema: ${error.message}`)
+}
+
+function forEachFileWithInterface(callback: (file: string, propsInterface: string, fileSource: string) => void) {
+  argv._.forEach(file => {
+    const source: string = fs.readFileSync(file, { encoding: 'utf-8' })
+    const propsInterface = generateRelayFragmentsInterface(schema, source)
+    if (propsInterface) callback(file, propsInterface, source)
+  })
+}
+
+if (argv.update) {
+  forEachFileWithInterface((file, propsInterface, source) => {
+    console.log(file)
+    const hasTrailingNewLine = source.endsWith("\n")
+    let result = source
+    if (!hasTrailingNewLine) {
+      result = result + '\n'
+    }
+    result = result + '\n' + propsInterface
+    if (hasTrailingNewLine) {
+      result = result + '\n'
+    }
+    fs.writeFileSync(file, result, { encoding: 'utf-8' })
+  })
+} else {
+  forEachFileWithInterface((file, propsInterface) => {
+    console.log(file)
+    console.log(propsInterface)
+    console.log('')
+  })
+}
