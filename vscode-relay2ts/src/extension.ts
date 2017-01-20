5133a73ebabfe83040cbccf19e30ac2b75fd706e
Eloy Durán <eloy.de.enige@gmail.com>
2017-01-20 01:25:29 -0500

    [vscode] Add commands to insert or append/replace.

--- vscode-relay2ts/src/extension.ts
+++ vscode-relay2ts/src/extension.ts
@@ -1,38 +1,91 @@
 import * as vscode from 'vscode'
 import * as path from 'path'
 import * as GraphQLConfigParser from 'graphql-config-parser'
-import { generateRelayFragmentsInterface } from 'relay2ts'
+import { GenerationResult, generateRelayFragmentsInterface } from 'relay2ts'
 import * as GraphQL from 'graphql'
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    try {
-      const config = GraphQLConfigParser.parse(vscode.workspace.rootPath)
-      if (config.file) {
-        config.file = path.resolve(vscode.workspace.rootPath, config.file)
-      }
-      GraphQLConfigParser.resolveSchema(config)
-        .then(schemaJSON => {
-          const schema = GraphQL.buildClientSchema(schemaJSON.data)
-          const source = vscode.window.activeTextEditor.document.getText()
-          const interfaceName = config.relay2ts && config.relay2ts.interfaceName
-          const generationResult = generateRelayFragmentsInterface(schema, source, interfaceName)
-          if (generationResult) {
-            vscode.window.activeTextEditor.edit(editor => {
-              editor.insert(vscode.window.activeTextEditor.selection.active, generationResult.propsInterface)
-            })
-          }
-        })
-        .catch(error => {
-          console.error(error)
-          vscode.window.showErrorMessage(error.message)
-        })
-    } catch(error) {
-      vscode.window.showErrorMessage(error.message)
-    }
-  });
+  const outputChannel = vscode.window.createOutputChannel('relay2ts')
 
-  context.subscriptions.push(disposable)
+  function loadConfig() {
+    return new Promise((resolve, reject) => {
+      try {
+        const config = GraphQLConfigParser.parse(vscode.workspace.rootPath)
+        if (config.file) {
+          config.file = path.resolve(vscode.workspace.rootPath, config.file)
+        }
+        resolve(config)
+      } catch(error) {
+        reject(error)
+      }
+    })
+  }
+
+  function loadSchema(
+    config: GraphQLConfigParser.Config
+  ): Promise<{ config: GraphQLConfigParser.Config, schema: GraphQL.GraphQLSchema }> {
+    return GraphQLConfigParser.resolveSchema(config)
+      .then(schemaJSON => {
+        outputChannel.appendLine(`Loaded GraphQL schema from ${config.file || config.request || config['graphql-js']}`)
+        return {
+          config,
+          schema: GraphQL.buildClientSchema(schemaJSON.data),
+        }
+      })
+  }
+
+  function generatePropsInterface(): Promise<GenerationResult> {
+    return loadConfig()
+      .then(loadSchema)
+      .then(({ config, schema }) => {
+        const source = vscode.window.activeTextEditor.document.getText()
+        const interfaceName = config.relay2ts && config.relay2ts.interfaceName
+        return generateRelayFragmentsInterface(schema, source, interfaceName)
+      })
+  }
+
+  function reportError(error: Error) {
+    outputChannel.appendLine(error.message)
+    outputChannel.appendLine(error.stack)
+    vscode.window.showErrorMessage(error.message)
+  }
+
+  context.subscriptions.push(vscode.commands.registerCommand('extension.printPropsInterface', () => {
+    generatePropsInterface()
+      .then(generationResult => {
+        if (generationResult) {
+          vscode.window.activeTextEditor.edit(editor => {
+            if (generationResult.existingInterfaceRange) {
+              const range = new vscode.Range(
+                vscode.window.activeTextEditor.document.positionAt(generationResult.existingInterfaceRange.start),
+                vscode.window.activeTextEditor.document.positionAt(generationResult.existingInterfaceRange.end),
+              )
+              editor.replace(range, generationResult.propsInterface)
+            } else {
+              const trailingNewLines = generationResult.input.match(/(\n*)$/)
+              const numberOfNewLines = trailingNewLines && trailingNewLines[1].length
+              const requiredNewLines = (numberOfNewLines && numberOfNewLines <= 2 && (2 - numberOfNewLines)) || 0
+              let append = `${'\n'.repeat(requiredNewLines)}${generationResult.propsInterface}`
+              const end = vscode.window.activeTextEditor.document.positionAt(generationResult.input.length)
+              editor.insert(end, append)
+            }
+          })
+        }
+      })
+      .catch(reportError)
+  }))
+
+  context.subscriptions.push(vscode.commands.registerCommand('extension.printPropsInterfaceAtCursor', () => {
+    generatePropsInterface()
+      .then(generationResult => {
+        if (generationResult) {
+          vscode.window.activeTextEditor.edit(editor => {
+            editor.insert(vscode.window.activeTextEditor.selection.active, generationResult.propsInterface)
+          })
+        }
+      })
+      .catch(reportError)
+  }))
 }
 
 export function deactivate() {
